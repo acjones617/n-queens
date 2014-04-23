dd2a7e8817e28f0b2cea8a8f9d394945dc2b60c4
Hack Reactor Students <acjones617@gmail.com>
2014-04-23 16:44:20 -0700

    Cleaned up helper functions

--- src/solvers.js
+++ src/solvers.js
@@ -1,45 +1,6 @@
 /*--------------------  Helper Functions  ---------------------*/
 
-
-var moreThanOnePiece = function(array, index) {
-  // return _.reduce(array, function(total, element) {
-  //   return total + element;
-  // }, 0) > 1;
-
-  for (var i = 0; i < array.length; i++) {
-    if (i !== index && array[i] === 1) {
-      return true;
-    }
-  };
-  return false;
-};
-
-// var returnBoard = function() {
-//   var board = this.attributes;
-//   var finalBoard = [];
-//   for (var i = 0; i < board.n; i++) {
-//     finalBoard.push(board[i]);
-//   }
-//   return finalBoard;
-// };
-
-
-// checking rows is unnecessary, will never put pieces on same row.
-var checkRowConflict = function(board, rowIndex, colIndex) {
-  return moreThanOnePiece(board[rowIndex], colIndex);
-};
-
-// should be unnecessary, will be checking in realtime.
-// var hasAnyRowConflicts = function() {
-//   for (var i = 0; i < this.attributes.n; i++) {
-//     if (this.hasRowConflictAt(i)) {
-//       return true;
-//     }
-//   }
-//   return false;
-// },
-
-// check any other pieces
+// check if any other pieces
 var noOtherPieces = function(array) {
   for (var i = 0; i < array.length; i++) {
     if (array[i] === 1) {
@@ -66,18 +27,6 @@ var noColConflict = function(board, rowIndex, colIndex) {
   return noOtherPieces(col);
 };
 
-// test if any columns on this board contain conflicts
-// should be unnecessary since checking in realtime
-// hasAnyColConflicts: function() {
-//   for (var i = 0; i < this.attributes.n; i++) {
-//     if (this.hasColConflictAt(i)) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
-
-
 
 // Major Diagonals - go from top-left to bottom-right
 // --------------------------------------------------------------
@@ -87,16 +36,6 @@ var noMajorDiagonalConflict = function(board, rowIndex, colIndex) {
   var majorDiag = [];
   var diagColIndex = colIndex - rowIndex;
 
-  // if (diagColIndex < 0) {
-  //   for (var i = -diagColIndex; i < this.attributes.n; i++) {
-  //     majorDiag.push(this.attributes[i][diagColIndex + i]);
-  //   }
-  // } else {
-  //   for (var i = 0; i < this.attributes.n - diagColIndex; i++) {
-  //     majorDiag.push(this.attributes[i][diagColIndex + i]);
-  //   }
-  // }
-  // in other words:
   // NOTE: board[0].length supposedly checks column count
   var maxIndex = Math.min(board.length, board[0].length - diagColIndex);
 
@@ -115,20 +54,6 @@ var noMajorDiagonalConflict = function(board, rowIndex, colIndex) {
 // [0,1,0,0,0,0],
 // [0,0,0,0,0,1]]
 
-// test if any major diagonals on this board contain conflicts
-// doesn't check trivial cases where diagonal only contains one square.
-// shouldn't be necessary, checking in real time.
-// hasAnyMajorDiagonalConflicts: function() {
-//   for (var i = -this.attributes.n + 2; i < this.attributes.n - 1; i++) {
-//     if (this.hasMajorDiagonalConflictAt(i)) {
-//       return true;
-//     }
-//   }
-//   return false;
-// };
-
-
-
 // Minor Diagonals - go from top-right to bottom-left
 // --------------------------------------------------------------
 //
@@ -138,16 +63,6 @@ var noMinorDiagonalConflict = function(board, rowIndex, colIndex) {
   var minorDiag = [];
   var diagColIndex = rowIndex + colIndex;
 
-  // if (diagColIndex >= this.attributes.n) {
-  //   for (var i = diagColIndex - this.attributes.n + 1; i < this.attributes.n; i++) {
-  //     minorDiag.push(this.attributes[i][diagColIndex - i]);
-  //   }
-  // } else {
-  //   for (var i = 0; i < diagColIndex + 1; i++) {
-  //     minorDiag.push(this.attributes[i][diagColIndex - i]);
-  //   }
-  // }
-  // in other words:
   var maxIndex = Math.min(diagColIndex + 1, board.length);
   // board[0].length should refer to num columns on board.
   for (var i = Math.max(0, diagColIndex - board[0].length + 1); i < maxIndex; i++) {
@@ -160,9 +75,6 @@ var noMinorDiagonalConflict = function(board, rowIndex, colIndex) {
   return noOtherPieces(minorDiag);
 };
 
-// test if any minor diagonals on this board contain conflicts
-// doesn't check the trivial cases where diagonal only contains one square;
-// Shouldn't be necessary anymore, checking in Real Time
 
 /*--------------------  End of Helper Functions  ---------------------*/
 
