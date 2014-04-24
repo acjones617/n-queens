16c432638822a3cce906cbf5b16fdc679e8abbc2
Hack Reactor Students <acjones617@gmail.com>
2014-04-23 23:14:14 -0700

    Got rid of breadcrumbs, updated to check column ahead of time. n = 12 is 378 ms

--- spec/solversSpec.js
+++ spec/solversSpec.js
@@ -54,30 +54,30 @@ describe('solvers', function() {
   // });
 
   describe('countNQueensSolutions() for large N', function() {
-    // it('n = 4', function() {
-    //   var solutionCount = countNQueensSolutions(4);
-    //   expect(solutionCount).to.be.equal(2);
-    // });
-
-    // it('n = 8', function() {
-    //   var solutionCount = countNQueensSolutions(8);
-    //   expect(solutionCount).to.be.equal(92);
-    // });
-
-    // it('n = 11', function() {
-    //   var solutionCount = countNQueensSolutions(11);
-    //   expect(solutionCount).to.be.equal(2680);
-    // });
-
-    // it('n = 12', function() {
-    //   var solutionCount = countNQueensSolutions(12);
-    //   expect(solutionCount).to.be.equal(14200);
-    // });
-
-    it('n = 13', function() {
-      var solutionCount = countNQueensSolutions(13);
-      expect(solutionCount).to.be.equal(73712);
+    it('n = 4', function() {
+      var solutionCount = countNQueensSolutions(4);
+      expect(solutionCount).to.be.equal(2);
     });
+
+    it('n = 8', function() {
+      var solutionCount = countNQueensSolutions(8);
+      expect(solutionCount).to.be.equal(92);
+    });
+
+    it('n = 11', function() {
+      var solutionCount = countNQueensSolutions(11);
+      expect(solutionCount).to.be.equal(2680);
+    });
+
+    it('n = 12', function() {
+      var solutionCount = countNQueensSolutions(12);
+      expect(solutionCount).to.be.equal(14200);
+    });
+
+    // it('n = 13', function() {
+    //   var solutionCount = countNQueensSolutions(13);
+    //   expect(solutionCount).to.be.equal(73712);
+    // });
 
   });
 
--- src/nQueens.js
+++ src/nQueens.js
@@ -1,25 +1,14 @@
-//NEXT STEP: Breadcrumb of diagonals.
+var noDiagonalConflict = function(board, next) {
+  var numberIndex = board.length;
+  var numberCheck = next;
 
-
-  // loop through array, given this one's "column index" (aka board.length - 1),
-  // lesser indices shouldn't be that number + deltaIndex for major, number - deltaIndex for minor
-
-var noDiagonalConflict = function(board) {
-  var numberIndex = board.length - 1;
-  var numberCheck = board[numberIndex];
-
-  for (var i = 0; i < board.length - 1; i++) {
+  for (var i = 0; i < board.length; i++) {
     if (numberCheck + (i - numberIndex) === board[i] || numberCheck - (i - numberIndex) === board[i]) {
       return false;
     }
   }
   return true;
 };
-
-// What will breadcrumb of diagonals look like?
-// {}
-//  When next col index is added, change appropriate colI (forbidden) down breadcrumb change to false
-
 
 window.countNQueensSolutions = function(n) {
   var num = n || 0;
@@ -34,39 +23,9 @@ window.countNQueensSolutions = function(n) {
     possibleRows.push(i);
   }
 
-  var forbiddenCol = [];
-  for (var i = 0; i < n; i++) {
-    forbiddenCol.push(true);
-  }
-  var diagCrumbs = [];
-  for (var i = 0; i < n; i++) {
-    diagCrumbs.push(forbiddenCol.slice());
-  }
-
-  var deepCopy = function(arr) {
-    var newArr = arr.slice();
-    for (var i = 0; i < arr.length; i++) {
-      newArr[i] = arr[i].slice();
-    }
-    return newArr;
-  };
-
-  var leaveCrumbs = function(crumbs, rowNum, colNum) {
-    var newCrumbs = deepCopy(crumbs);
-    for (var i = 1; i < n - rowNum; i++) {
-      if (colNum + i < n) {
-        newCrumbs[rowNum+i][colNum+i] = false;
-      }
-      if (colNum - i >= 0) {
-        newCrumbs[rowNum+i][colNum-i] = false;
-      }
-    }
-    return newCrumbs;
-  };
-
   // recursively put "queen" on next "row" (aka array cell), in specific "columns" (aka the number in the array cell)
   // If it doesn't diagonally conflict, then continue recursion, otherwise, cut off recursion
-  var findSolutions = function(semiBoard, possibleColsLeft, breadCrumbs) {
+  var findSolutions = function(semiBoard, possibleColsLeft) {
     var numsToCheck;
     var currentLengthBoard = semiBoard.length;
 
@@ -87,31 +46,21 @@ window.countNQueensSolutions = function(n) {
       if (currentLengthBoard === 0) {
         numsToCheck /= 2;
       }
-      // don't even bother checking if index is immediate diagonal of lastIndex
-      // or if index is +-2 diagonal of lastLastIndex
       for (var i = 0; i < numsToCheck; i++) {
         // check upfront to make sure diagonal is free
-        // sending in row/col into breadCrumbs array check
-        // we are currently checking on putting num into row = currentLengthBoard index
-        // we are looking to see if we can place the column number = possibleColsLeft[i];
-        //console.log(currentLengthBoard, breadCrumbs);
-        if (breadCrumbs[currentLengthBoard][possibleColsLeft[i]]) {
+        if (noDiagonalConflict(semiBoard, possibleColsLeft[i])) {
         // create copy of semiBoard so we don't mess with it.
           var newBoard = semiBoard.slice();
           var colsLeft = possibleColsLeft.slice();
           var nextColNum = colsLeft.splice(i,1)[0];
-
-          if (colsLeft.length >= 1) {
-            var newBreadCrumbs = leaveCrumbs(breadCrumbs, semiBoard.length, nextColNum);
-          }
           newBoard.push(nextColNum);
-          findSolutions(newBoard, colsLeft, newBreadCrumbs);
+          findSolutions(newBoard, colsLeft);
         }
       }
     }
   };
 
-  findSolutions([], possibleRows, diagCrumbs);
+  findSolutions([], possibleRows);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
