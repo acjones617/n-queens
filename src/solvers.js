ea97ccb9fe0f669f680349abfbf63e0fb0d50bb3
Hack Reactor Students <acjones617@gmail.com>
2014-04-23 20:39:29 -0700

    Now dealing with numeric indices rather than nested arrays.

--- src/solvers.js
+++ src/solvers.js
@@ -1,24 +1,7 @@
-
 var makeEmptyRow = function(n) {
   return _(_.range(n)).map(function() {
-    return false;
+    return 0;
   });
-};
-
-var makeEmptyMatrix = function(n) {
-  return _(_.range(n)).map(function() {
-    return _(_.range(n)).map(function() {
-      return false;
-    });
-  });
-};
-
-var makePossibleRows = function(n) {
-  var rows = makeEmptyMatrix(n);
-  for (var i = 0; i < n; i++) {
-    rows[i][i] = true;
-  }
-  return rows;
 };
 
 ////////////////// QUEEN HELPER TO SEE WHAT'S GOING ON ////////////////////////
@@ -209,55 +192,25 @@ window.findNQueensSolution = function(n) {
 // COLUMNS - run from top to bottom
 // --------------------------------------------------------------
 //
-// test if a specific column on this board contains a conflict
-var noColConflict = function(board, rowIndex, colIndex) {
-  // board[0].length should refer to num columns on board.
-  // minus 1 on the "i bound" since we'll always be checking against placing one on the last row,
-  // and we don't want to check that one. We are checking conflicts against that.
-  for (var i = 0; i < board.length - 1; i++) {
-    // don't check row our current piece is on
-    if (board[i][colIndex] === true) {
-      return false;
-    }
-  }
-  return true;
-};
+// won't have to do columns now, just won't add previously added number (combinatoral now)
 
 
 // Major Diagonals - go from top-left to bottom-right
 // --------------------------------------------------------------
 //
-// test if a specific major diagonal on this board contains a conflict
-var noMajorDiagonalConflict = function(board, rowIndex, colIndex) {
-  var diagColIndex = colIndex - rowIndex;
-  var maxIndex = Math.min(board.length, board[0].length - diagColIndex);
-  // check noColConflict for notes
-  for (var i = -Math.min(diagColIndex,0); i < maxIndex - 1; i++) {
-    if (board[i][diagColIndex + i] === true) {
+// loop through array, given this one's "column index" (aka board.length - 1),
+// lesser indices shouldn't be that number + deltaIndex for major, number - deltaIndex for minor
+var noDiagonalConflict = function(board) {
+  var numberIndex = board.length - 1;
+  var numberCheck = board[numberIndex];
+
+  for (var i = 0; i < board.length - 1; i++) {
+    if (numberCheck + (i - numberIndex) === board[i] || numberCheck - (i - numberIndex) === board[i]) {
       return false;
     }
   }
   return true;
 };
-
-// Minor Diagonals - go from top-right to bottom-left
-// --------------------------------------------------------------
-//
-// test if a specific minor diagonal on this board contains a conflict
-
-var noMinorDiagonalConflict = function(board, rowIndex, colIndex) {
-  var diagColIndex = rowIndex + colIndex;
-
-  var maxIndex = Math.min(diagColIndex + 1, board.length);
-  // check noColConflict for notes
-  for (var i = Math.max(0, diagColIndex - board[0].length + 1); i < maxIndex - 1; i++) {
-    if (board[i][diagColIndex - i] === true){
-      return false;
-    }
-  }
-  return true;
-};
-
 
 /*--------------------  End of Helper Functions  ---------------------*/
 
@@ -267,51 +220,49 @@ var noMinorDiagonalConflict = function(board, rowIndex, colIndex) {
 window.countNQueensSolutions = function(n) {
   var num = n || 0;
   var solutionCount = 0;
-  // never put rooks on same row, treat this as similar to the rock/paper/scissors except each rounds are rows.
-  // recursively put rook on next row, check if colConflict.
-  // If not, then continue recursion, otherwise, cut off recursion
+  // recursively put "queen" on next "row" (aka array cell), in specific "columns" (aka the number in the array cell)
+  // If it doesn't diagonally conflict, then continue recursion, otherwise, cut off recursion
 
-  //possible rows are arrays of length num, with a single 1 in them.
-  var possibleRows = makePossibleRows(num);
+  //possible rows is just a range of numbers from 0 to n now.
+  var possibleRows = [];
+  for (var i = 0; i < n; i++) {
+    possibleRows.push(i);
+  }
 
-  // now generate one solutions:
-
-  var findASolution = function(semiBoard, num, lastIndex, lastLastIndex) {
+  // now generate solutions:
+  var findSolutions = function(semiBoard, possibleRowsLeft) {
     var rowsToCheck;
-    if (semiBoard.length === num) {
+
+    if (semiBoard.length === n) {
       // count found solution and the mirrored solution,
       // if top element not in center, else just count 1.
-      if (num % 2 === 1 && semiBoard[0][(num-1)/2]) {
-        solutionCount++;
-      } else {
-        solutionCount += 2;
-      }
+      // if (num % 2 === 1 && semiBoard[0][(num-1)/2]) {
+      solutionCount++;
+      // } else {
+      //   solutionCount += 2;
+
       return;
     } else {
       // for first row, we only want to add half of possible Rows
       // then, double count solutions (count mirrored solutions)
-      rowsToCheck = possibleRows.length;
-      if (semiBoard.length === 0) {
-        rowsToCheck = Math.ceil(num/2);
-      }
+      rowsToCheck = possibleRowsLeft.length;
+      // if (semiBoard.length === 0) {
+      //   rowsToCheck = Math.ceil(num/2);
+      // }
+
       for (var i = 0; i < rowsToCheck; i++) {
-        if (i !== lastIndex && i !== lastIndex - 1 && i !== lastIndex + 1 && i !== lastLastIndex - 2 && i !== lastLastIndex && i !== lastLastIndex + 2) {
-          var newSemiBoard = semiBoard.slice();
-          newSemiBoard.push(possibleRows[i]);
-          // i = col on which new queen/rook was added
-          // newSemiBoard.length - 1 = row on which new queen/rook was added
-          // if already conflicts, scrap it, don't run recursion on it.
-          if (noColConflict(newSemiBoard, newSemiBoard.length - 1, i) &&
-            noMajorDiagonalConflict(newSemiBoard, newSemiBoard.length - 1, i) &&
-            noMinorDiagonalConflict(newSemiBoard, newSemiBoard.length - 1, i)){
-            findASolution(newSemiBoard, num, i, lastIndex, lastLastIndex);
-          }
+        // create copy of semiBoard so we don't mess with it.
+        var newBoard = semiBoard.slice();
+        var rowsLeft = possibleRowsLeft.slice();
+        newBoard.push(rowsLeft.splice(i,1)[0]);
+        if (noDiagonalConflict(newBoard)) {
+          findSolutions(newBoard, rowsLeft);
         }
       }
     }
   };
 
-  findASolution([], num);
+  findSolutions([], possibleRows);
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
